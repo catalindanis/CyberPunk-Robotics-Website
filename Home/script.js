e97c0e6d2200945dc29efb5f057300890c4c2d04
Catalin <geaigelsaflu@gmail.com>
2022-10-25 11:59:24 +0100

    Resolved bugs

--- Home/script.js
+++ Home/script.js
@@ -1,10 +1,12 @@
 window.addEventListener('load', function () {
     init();
-})
+});
 
 function init(){
     document.getElementsByClassName("root")[0].style.display = "block";
 }
+
+var menuIsOpened = false;
 
 function openNavigationBar(){
     document.getElementsByClassName("menu")[0].style.display = "block";
@@ -13,6 +15,7 @@ function openNavigationBar(){
     document.getElementsByClassName("logo")[0].style.opacity = "10%";
     document.getElementsByClassName("text")[0].style.opacity = "10%";
     document.getElementsByClassName("social_media")[0].style.opacity = "10%";
+    menuIsOpened = true;
 }
 
 function closeNavigationBar(){
@@ -22,6 +25,26 @@ function closeNavigationBar(){
     document.getElementsByClassName("text")[0].style.opacity = "100%";
     document.getElementsByClassName("social_media")[0].style.opacity = "100%";
     setTimeout( function() { document.getElementsByClassName("menu")[0].style.display = "none"; }, 1000);
+    menuIsOpened = false;
 }
 
+
+window.addEventListener("resize", function() {
+    closeMenuIfNeeded();
+});
+
+function closeMenuIfNeeded(){
+    if(window.innerWidth >= 990 && menuIsOpened)
+        closeNavigationBar();
+}
+
+window.addEventListener("scroll", preventMotion, false);
+window.addEventListener("touchmove", preventMotion, false);
+
+function preventMotion(event)
+{
+    window.scrollTo(0, 0);
+    event.preventDefault();
+    event.stopPropagation();
+}
 
