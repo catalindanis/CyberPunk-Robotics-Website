04c2dac32f2be2b93a67548644d4661c817e6eab
Catalin <geaigelsaflu@gmail.com>
2022-10-30 01:20:37 +0100

    Started Page 'Despre Noi'

--- Despre Noi/script.js
+++ Despre Noi/script.js
@@ -1,20 +1,36 @@
 window.addEventListener('load', function () {
-    checkDeviceType();
-})
+    init();
+});
 
-function checkDeviceType(){
-    if(/Android|webOS|iPhone|BlackBerry|IEMobile/i.test(navigator.userAgent)){
-        mobileInit();
-    }
-    else{
-        desktopInit();
-    }
+function init(){
+    document.getElementsByClassName("root")[0].style.display = "block";
 }
 
-function mobileInit(){
-    document.getElementsByClassName("mobile")[0].style.display = "block";
+var menuIsOpened = false;
+
+function openNavigationBar(){
+    document.getElementsByClassName("menu")[0].style.display = "block";
+    document.getElementsByClassName("menu")[0].style.animation = "load_menu_mobile 1s";
+    document.getElementsByClassName("logo")[0].style.opacity = "10%";
+    document.getElementsByClassName("informations")[0].style.opacity = "10%";
+    menuIsOpened = true;
 }
 
-function desktopInit(){
-    document.getElementsByClassName("desktop")[0].style.display = "block";
+function closeNavigationBar(){
+    document.getElementsByClassName("menu")[0].style.animation = "close_menu_mobile 1s";
+    document.getElementsByClassName("logo")[0].style.opacity = "100%";
+    document.getElementsByClassName("informations")[0].style.opacity = "100%";
+    setTimeout( function() { document.getElementsByClassName("menu")[0].style.display = "none"; }, 1000);
+    menuIsOpened = false;
 }
+
+
+window.addEventListener("resize", function() {
+    closeMenuIfNeeded();
+});
+
+function closeMenuIfNeeded(){
+    if(window.innerWidth >= 990 && menuIsOpened)
+        closeNavigationBar();
+}
+
